1ba5abbfd7e0f3e241f8b40558ec08c73e0d2028
dingjunjie <864546065@qq.com>
2023-05-23 00:07:00 +0800

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,84 @@
+// ** Next Imports
+import Head from 'next/head'
+import { Router } from 'next/router'
+import type { NextPage } from 'next'
+import type { AppProps } from 'next/app'
+
+// ** Loader Import
+import NProgress from 'nprogress'
+
+// ** Emotion Imports
+import { CacheProvider } from '@emotion/react'
+import type { EmotionCache } from '@emotion/cache'
+
+// ** Config Imports
+import themeConfig from 'src/configs/themeConfig'
+
+// ** Component Imports
+import UserLayout from 'src/layouts/UserLayout'
+import ThemeComponent from 'src/@core/theme/ThemeComponent'
+
+// ** Contexts
+import { SettingsConsumer, SettingsProvider } from 'src/@core/context/settingsContext'
+
+// ** Utils Imports
+import { createEmotionCache } from 'src/@core/utils/create-emotion-cache'
+
+// ** React Perfect Scrollbar Style
+import 'react-perfect-scrollbar/dist/css/styles.css'
+
+// ** Global css styles
+import '../../styles/globals.css'
+
+// ** Extend App Props with Emotion
+type ExtendedAppProps = AppProps & {
+  Component: NextPage
+  emotionCache: EmotionCache
+}
+
+const clientSideEmotionCache = createEmotionCache()
+
+// ** Pace Loader
+if (themeConfig.routingLoader) {
+  Router.events.on('routeChangeStart', () => {
+    NProgress.start()
+  })
+  Router.events.on('routeChangeError', () => {
+    NProgress.done()
+  })
+  Router.events.on('routeChangeComplete', () => {
+    NProgress.done()
+  })
+}
+
+// ** Configure JSS & ClassName
+const App = (props: ExtendedAppProps) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+
+  // Variables
+  const getLayout = Component.getLayout ?? (page => <UserLayout>{page}</UserLayout>)
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>{`${themeConfig.templateName} - DING AI`}</title>
+        <meta
+          name='description'
+          content={`${themeConfig.templateName} – DING AI是基于openai的人工智能聊天网站，提供智能聊天、语音识别、机器翻译等服务。DING AI致力于为用户提供最优质的智能互动体验，让您的生活更加轻松便利.`}
+        />
+        <meta name='keywords' content='openai, 人工智能, 智能聊天, 语音识别, 机器翻译, DING AI' />
+        <meta name='viewport' content='initial-scale=1, width=device-width' />
+      </Head>
+
+      <SettingsProvider>
+        <SettingsConsumer>
+          {({ settings }) => {
+            return <ThemeComponent settings={settings}>{getLayout(<Component {...pageProps} />)}</ThemeComponent>
+          }}
+        </SettingsConsumer>
+      </SettingsProvider>
+    </CacheProvider>
+  )
+}
+
+export default App
